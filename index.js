e1f48bc4f73f94e07dc2081306f8a12e1acc27b6
lakca <912910011@qq.com>
2020-12-14 11:13:14 +0800

    refactor watching files: watching related files automatically.

--- index.js
+++ index.js
@@ -59,101 +59,189 @@ const { watch } = require('chokidar')
  * @param {string|string[]} [options.watch]
  *  Watch mock folders/files to refresh mock routes.
  *  if any folder will effect the mock data, it should be included.
+ *
+ * @param {RegExp} [options.ignore]
+ * ignore pattern.
+ *
+ * @param {boolean} [options.silent]
+ * ignore pattern.
+ *
+ * @return {function (import('express').Application): void}
  */
 module.exports = options => {
   const routeFiles = Array.isArray(options.routeFiles) ? options.routeFiles : [options.routeFiles]
+  const ignoreRegExp = options.ignore
   return app => {
     app.lazyrouter()
+    const watcher = watch([], { ignoreInitial: true })
     const stack = app._router.stack
-    const anchor = stack.length
-    let count = mountRoutes(app)
-    watch(options.watch, { ignoreInitial: true })
-      .on('all', (event, filepath) => {
-        stack.splice(anchor, count)
-        const start = stack.length
-        delete require.cache[filepath]
-        try {
-          count = mountRoutes(app)
-        } catch (e) {
-          console.error(e)
-          count = 0
-          app.emit('routeReloadFailed')
-          return
+    const addedLayers = []
+    let anchor = -1
+    mountRoutes()
+    watcher.on('all', (event, filepath) => {
+      unwatchRequireChain(filepath)
+      clearRequireCache(filepath)
+      try {
+        mountRoutes()
+      } catch (e) {
+        if (options.silent) {
+          app.emit('reloadRouteFailed', e)
+        } else {
+          throw e
         }
-        // move to the original position.
-        stack.splice(anchor, 0, ...stack.splice(start, count))
-        app.emit('routeReloaded')
-      })
-  }
-  /**
-   * mount routes.
-   *
-   * @author lakca<912910011@qq.com>
-   * @param {import('express').Application} app
-   * @returns
-   */
-  function mountRoutes(app) {
-    /** @type {RouteItem[]} */
-    const items = []
-    for (const file of routeFiles) {
-      items.push.apply(items, require(file))
-    }
-    for (const item of items) {
-      let method, url, response, range, delay, args, processResponse, j
-      if (Array.isArray(item)) {
-        switch (item.length) {
-          case 0:
-          case 1:
-            continue
-          case 2:
-            [url, response] = item // get
-            break
-          case 3:
-            [method, url, response] = item
-            break
-          default:
-            [method, url, response] = item
-            j = 3
-            while (j < item.length) {
-              switch (typeof item[j]) {
-                case 'function':
-                  processResponse = item[j]
+        return
+      }
+      app.emit('reloadRouteSuccess')
+    })
+
+    /**
+     * 1. remove previous added routes.
+     * 2. reset `anchor` and `routeCount`.
+     * 3. add new routes one by one with increasing `routeCount`.
+     * 4. rebase added routes to original position.
+     */
+    function mountRoutes() {
+      // remove previous added routes.
+      for (let i = addedLayers.length; i--;) {
+        const layer = addedLayers[i]
+        const index = stack.indexOf(layer)
+        if (~index) {
+          stack.splice(index, 1)
+          anchor = index
+        }
+      }
+      // empty saved layers.
+      addedLayers.length = 0
+      // add new routes one by one with updating 'routeCount' in the meantime.
+      let err
+      try {
+        for (const file of routeFiles) {
+          const definitions = loadRouteModule(file)
+          for (const def of definitions) {
+            let method, url, response, range, delay, args, processResponse
+            if (Array.isArray(def)) {
+              let j
+              switch (def.length) {
+                case 0:
+                case 1:
+                  continue
+                case 2:
+                  [url, response] = def
                   break
-                case 'object':
-                  args = item[j]
-                  break
-                case 'string':
-                  range = item[j]
-                  break
-                case 'number':
-                  delay = item[j]
+                case 3:
+                  [method, url, response] = def
                   break
                 default:
+                  [method, url, response] = def
+                  processResponse = []
+                  j = 3
+                  while (j < def.length) {
+                    switch (typeof def[j]) {
+                      case 'function':
+                        processResponse.push(def[j])
+                        break
+                      case 'object':
+                        args = def[j]
+                        break
+                      case 'string':
+                        range = def[j]
+                        break
+                      case 'number':
+                        delay = def[j]
+                        break
+                      default:
+                    }
+                    j++
+                  }
               }
-              j++
+            } else if (def) {
+              method = def.method
+              url = def.url
+              response = def.response
+              range = def.range
+              delay = def.delay
+              args = def.args
+              processResponse = Array.isArray(def.processResponse) ? def.processResponse : [def.processResponse]
+            } else {
+              continue
             }
-        }
-      } else {
-        ({ method, url, response, range, delay, args, processResponse } = item)
-      }
-      app[method || 'get'](url, function(req, res) {
-        const count = range ? random(range) : 1
-        let r = typeof response === 'function'
-          ? response(req, res, { count, render, mock, delayCall })
-          : range
-            ? new Array(count).fill(null).map((e, i) => mock(render(response, Object.assign({ req, i }, args))))
-            : mock(render(response, Object.assign({ req }, args)))
-        if (r === res) {
-          delayCall(delay, () => res.end())
-        } else {
-          if (processResponse) {
-            r = processResponse(r, req, res, { count, render, mock, delayCall })
+            processResponse = processResponse.filter(e => typeof e === 'function')
+            app[method || 'get'](url, function(req, res) {
+              const count = range ? random(range) : 1
+              let r
+              if (typeof response === 'function') {
+                r = response(req, res, { count, render, mock, delayCall })
+              } else if (range) {
+                r = new Array(count).fill(null).map((e, i) => mock(render(response, Object.assign({ req, i }, args))))
+              } else {
+                r = mock(render(response, Object.assign({ req }, args)))
+              }
+
+              processResponse.forEach(e => {
+                r = e(r, req, res, { count, render, mock, delayCall })
+              })
+
+              if (r === res) {
+                delayCall(delay, () => res.end())
+              } else if (r !== void 0) {
+                delayCall(delay, () => res.send(r))
+              }
+            })
+            // save new layers
+            addedLayers.push(stack.pop())
           }
-          delayCall(delay, () => res.send(r))
+        }
+      } catch (e) {
+        err = e
+      }
+      // add new layers
+      if (~anchor) {
+        stack.splice(anchor, 0, ...addedLayers)
+      } else {
+        stack.push(...addedLayers)
+      }
+      if (err) {
+        throw err
+      }
+    }
+
+    function loadRouteModule(file) {
+      try {
+        const definitions = require(file)
+        watchRequireChain(file)
+        return definitions
+      } catch (e) {
+        watchRequireChain(file)
+        throw e
+      }
+    }
+
+    function watchRequireChain(id) {
+      eachRequire(id, function(mod) {
+        if (ignoreRegExp && ignoreRegExp.test(mod.id)) {
+          return
+        }
+        watcher.add(mod.id)
+      })
+    }
+
+    function unwatchRequireChain(id) {
+      eachRequire(id, function(mod) {
+        if (mod.id !== id) {
+          watcher.unwatch(mod.id)
         }
       })
     }
-    return items.length
+
+    // delete cache from entry(route file) to changed file
+    function clearRequireCache(id) {
+      if (require.cache[id]) {
+        if (require.cache[id].parent) {
+          clearRequireCache(require.cache[id].parent.id)
+        }
+        delete require.cache[id]
+      }
+    }
   }
 }
 
@@ -224,4 +312,18 @@ function delayCall(seconds, cb) {
     seconds = Math.random() * (seconds[1] - seconds[0]) + seconds[0]
   }
   setTimeout(cb, seconds * 1000)
+}
+
+function eachRequire(id, handler) {
+  const mod = require.cache[id]
+  if (mod) {
+    const terminated = handler(mod)
+    if (!terminated) {
+      if (mod.children) {
+        mod.children.forEach(childMod => {
+          eachRequire(childMod.id, handler)
+        })
+      }
+    }
+  }
 }
